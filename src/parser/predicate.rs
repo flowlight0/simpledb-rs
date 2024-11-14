067e3003d8876c8bb7fb980e48a03e093f6ae8d0
Takanori Hayashi <flowlight0@gmail.com>
2024-11-15 03:20:17 +0900

    Implement select scan

--- src/parser/predicate.rs
+++ src/parser/predicate.rs
@@ -21,6 +21,21 @@ impl Expression {
             }
         }
     }
+
+    pub fn try_get_field(&self) -> Option<&str> {
+        match self {
+            Expression::Field(field_name) => Some(field_name),
+            _ => None,
+        }
+    }
+
+    pub fn try_get_constant(&self) -> Option<Value> {
+        match self {
+            Expression::I32Constant(value) => Some(Value::I32(*value)),
+            Expression::StringConstant(value) => Some(Value::String(value.clone())),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -40,7 +55,77 @@ impl Term {
     }
 
     pub fn get_reduction_factor(&self, plan: &Box<dyn Plan>) -> usize {
-        todo!()
+        match self {
+            Term::Equality(lhs, rhs) => {
+                let lhs_field = lhs.try_get_field();
+                let rhs_field = rhs.try_get_field();
+                match (lhs_field, rhs_field) {
+                    (Some(lhs_field), Some(rhs_field)) => {
+                        let l_distinct = plan.num_distinct_values(lhs_field);
+                        let r_distinct = plan.num_distinct_values(rhs_field);
+                        return std::cmp::max(l_distinct, r_distinct);
+                    }
+                    (Some(lhs_field), None) => {
+                        return plan.num_distinct_values(lhs_field);
+                    }
+                    (None, Some(rhs_field)) => {
+                        return plan.num_distinct_values(rhs_field);
+                    }
+                    _ => {}
+                }
+                if lhs == rhs {
+                    return 1;
+                } else {
+                    // Just return some relatively large number.
+                    // Integer.MAX_VALUE was used in the book, but I don't use it to avoid overflow.
+                    return 100;
+                }
+            }
+        }
+    }
+
+    fn equates_with_constant(&self, field_name: &str) -> Option<Value> {
+        match self {
+            Term::Equality(lhs, rhs) => {
+                if let Some(lhs_field) = lhs.try_get_field() {
+                    if lhs_field == field_name {
+                        if let Some(rhs_constant) = rhs.try_get_constant() {
+                            return Some(rhs_constant);
+                        }
+                    }
+                }
+                if let Some(rhs_field) = rhs.try_get_field() {
+                    if rhs_field == field_name {
+                        if let Some(lhs_constant) = lhs.try_get_constant() {
+                            return Some(lhs_constant);
+                        }
+                    }
+                }
+            }
+        }
+        return None;
+    }
+
+    fn equates_with_field(&self, field_name: &str) -> Option<String> {
+        match self {
+            Term::Equality(lhs, rhs) => {
+                if let Some(lhs_field) = lhs.try_get_field() {
+                    if lhs_field == field_name {
+                        if let Some(rhs_field) = rhs.try_get_field() {
+                            return Some(rhs_field.to_string());
+                        }
+                    }
+                }
+                if let Some(rhs_field) = rhs.try_get_field() {
+                    if rhs_field == field_name {
+                        if let Some(lhs_field) = lhs.try_get_field() {
+                            return Some(lhs_field.to_string());
+                        }
+                    }
+                }
+            }
+        }
+        return None;
     }
 }
 
@@ -69,5 +154,25 @@ impl Predicate {
             factor *= term.get_reduction_factor(plan);
         }
         factor
+    }
+
+    pub(crate) fn equates_with_constant(&self, field_name: &str) -> Option<Value> {
+        for term in &self.terms {
+            let t = term.equates_with_constant(field_name);
+            if t.is_some() {
+                return t;
+            }
+        }
+        None
+    }
+
+    pub(crate) fn equates_with_field(&self, field_name: &str) -> Option<String> {
+        for term in &self.terms {
+            let t = term.equates_with_field(field_name);
+            if t.is_some() {
+                return t;
+            }
+        }
+        None
     }
 }
---
+++ src/plan/select_plan.rs
@@ -0,0 +1,118 @@
+use std::sync::{Arc, Mutex};
+
+use crate::{
+    parser::predicate::Predicate, record::schema::Schema, scan::select_scan::SelectScan,
+    tx::transaction::Transaction,
+};
+
+use super::Plan;
+
+pub struct SelectPlan {
+    plan: Box<dyn Plan>,
+    predicate: Predicate,
+}
+
+impl SelectPlan {
+    pub fn new(plan: Box<dyn Plan>, predicate: Predicate) -> Self {
+        SelectPlan { plan, predicate }
+    }
+}
+
+impl Plan for SelectPlan {
+    fn get_num_accessed_blocks(&self) -> usize {
+        self.plan.get_num_accessed_blocks()
+    }
+
+    fn get_num_output_records(&self) -> usize {
+        self.plan.get_num_output_records() / self.predicate.get_reduction_factor(&self.plan)
+    }
+
+    fn num_distinct_values(&self, field_name: &str) -> usize {
+        if self.predicate.equates_with_constant(field_name).is_some() {
+            1
+        } else {
+            let another_field = self.predicate.equates_with_field(field_name);
+            if another_field.is_some() {
+                let l_distinct = self.plan.num_distinct_values(field_name);
+                let r_distinct = self.plan.num_distinct_values(&another_field.unwrap());
+                std::cmp::min(l_distinct, r_distinct)
+            } else {
+                self.plan.num_distinct_values(field_name)
+            }
+        }
+    }
+
+    fn schema(&self) -> Schema {
+        self.plan.schema()
+    }
+
+    fn open(
+        &mut self,
+        tx: Arc<Mutex<Transaction>>,
+    ) -> Result<Box<dyn crate::scan::Scan>, anyhow::Error> {
+        let base_scan = self.plan.open(tx)?;
+        Ok(Box::new(SelectScan::new(base_scan, self.predicate.clone())))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use super::*;
+    use crate::db::SimpleDB;
+    use crate::parser::predicate::{Expression, Term};
+    use crate::plan::select_plan;
+    use crate::plan::table_plan::TablePlan;
+    use crate::record::layout::Layout;
+    use crate::record::schema::Schema;
+    use crate::scan::table_scan::TableScan;
+    use crate::scan::Scan;
+
+    #[test]
+    fn test_select_plan() -> Result<(), anyhow::Error> {
+        let temp_dir = tempfile::tempdir().unwrap().into_path().join("directory");
+        let block_size = 256;
+        let num_buffers = 3;
+        let mut db = SimpleDB::new(temp_dir, block_size, num_buffers)?;
+
+        let mut schema = Schema::new();
+        schema.add_i32_field("A");
+        schema.add_string_field("B", 20);
+        let layout = Rc::new(Layout::new(schema));
+
+        let table_name = "testtable";
+        let tx = Arc::new(Mutex::new(db.new_transaction()?));
+
+        db.metadata_manager
+            .create_table(table_name, &layout.schema, tx.clone())?;
+        let mut table_scan = TableScan::new(tx.clone(), table_name, layout.clone())?;
+        table_scan.before_first()?;
+        for i in 0..200 {
+            table_scan.insert()?;
+            table_scan.set_i32("A", i)?;
+            table_scan.set_string("B", &i.to_string())?;
+        }
+        drop(table_scan);
+        tx.lock().unwrap().commit()?;
+
+        let tx = Arc::new(Mutex::new(db.new_transaction()?));
+        db.metadata_manager
+            .stat_manager
+            .refresh_statistics(tx.clone())?;
+        let table_plan = TablePlan::new(tx.clone(), table_name, &mut db.metadata_manager)?;
+        let select_plan = select_plan::SelectPlan::new(
+            Box::new(table_plan),
+            Predicate::new(vec![Term::Equality(
+                Expression::Field("A".to_string()),
+                Expression::I32Constant(1),
+            )]),
+        );
+
+        assert!(select_plan.get_num_accessed_blocks() > 0);
+        assert!(select_plan.get_num_output_records() < 200);
+        assert!(select_plan.num_distinct_values("A") > 0);
+
+        Ok(())
+    }
+}
