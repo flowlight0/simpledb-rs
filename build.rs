6063610876e5d27212f375c31ad213b4e9ffad57
Takanori Hayashi <flowlight0@gmail.com>
2024-12-01 16:41:21 +0900

    Define RPC service with proto

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
-fn main() {
+use std::{env, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/proto");
+    tonic_build::configure()
+        .out_dir(out_dir)
+        .compile_protos(&["proto/simpledb.proto"], &["proto"])?;
+
     lalrpop::Configuration::new()
         .generate_in_source_tree()
         .process()
-        .unwrap();
 }
