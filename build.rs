6d02a4133e5f15f3747837c16a91679cc8d6f088
Takanori Hayashi <flowlight0@gmail.com>
2025-06-18 20:42:16 +0900

    Format generated sources in build script (#87)

--- build.rs
+++ build.rs
@@ -1,12 +1,26 @@
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, process::Command};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/proto");
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
+    let out_dir = PathBuf::from(&manifest_dir).join("src/proto");
     tonic_build::configure()
-        .out_dir(out_dir)
+        .out_dir(&out_dir)
         .compile_protos(&["proto/simpledb.proto"], &["proto"])?;
 
     lalrpop::Configuration::new()
         .generate_in_source_tree()
-        .process()
+        .process()?;
+
+    // Format auto-generated sources.
+    Command::new("cargo")
+        .current_dir(&manifest_dir)
+        .args([
+            "fmt",
+            "--",
+            "src/proto/simpledb.rs",
+            "src/parser/grammar.rs",
+        ])
+        .status()?;
+
+    Ok(())
 }
